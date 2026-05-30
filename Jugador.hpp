662087d48165152e8495f38e6ee4e740cd1d7a82
Lasardillascrack <lasardillascrac2026@gmail.com>
2026-05-29 22:07:02 -0400

    Update Jugador.hpp
    Added PuntajeBalatro struct and updated Jugador class methods.

--- Jugador.hpp
+++ Jugador.hpp
@@ -5,6 +5,12 @@
 #include <vector>
 #include "Carta.hpp"
 
+struct PuntajeBalatro {
+    std::string nombreMano;
+    int fichasBase;
+    int multBase;
+};
+
 class Jugador {
 private:
     std::string nombreJugador;
@@ -12,19 +18,16 @@ private:
 
 public:
     Jugador(std::string _nombreJugador);
-    
+
     std::string obtenerNombre() const;
     void recibirCarta(const Carta& nuevaCarta);
     bool descartarPorIndice(int posicionIndice);
-    
-    // Métodos avanzados para procesar la interfaz que exige la foto 1000218954_2.jpg
-    int calcularPuntajeFichas() const;
-    int calcularMultiplicador() const;
-    std::string detectarJugada() const;
-    
+
+    Carta obtenerCartaEn(int indice) const; // Permite extraer una carta específica para jugarla
+    PuntajeBalatro evaluarJugadaExacta(const std::vector<Carta>& jugada) const; //evaluador
+
     int obtenerCantidadCartas() const;
     void mostrarMano() const;
     void limpiarMano();
 };
-
 #endif
